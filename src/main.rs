ab59d2197c92cc65635f8077408ac54197208672
blu <52867612+hueblu@users.noreply.github.com>
2022-11-21 20:05:32 -0800

    vga buffer text buffer firsrt iteration of vga text buffer api for writing to the screen

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 #![no_std]
 #![no_main]
 
-static HELLO: &[u8] = b"Hello Father!";
+extern crate lazy_static;
+extern crate spin;
+extern crate volatile;
+
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello F{}t{}er", "a", "h");
+    print!("Hello ");
+    println!("Again");
+    panic!("Panicking panicked");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
